6c5f51f8397a7af20cd7f003f2b525f780817acd
luocaixin <879410270@qq.com>
2018-01-10 19:30:33 +0800

    with lua

---
+++ LuaAdapter/Test/main.cpp
@@ -0,0 +1,31 @@
+
+#include "../Classes/LuaAdapter.hpp"
+#include "test_center.h"
+
+static int __luaErrorHook(lua_State* L) {
+
+	const string errorStr = lua_tostring(L, -1);
+
+	cout << errorStr << endl;
+	lua_Debug info;
+	int level = 0;
+	while (lua_getstack(L, level, &info)) {
+		lua_getinfo(L, "nSl", &info);
+		fprintf(stderr, "  [%d] %s:%d -- %s [%s]\n",
+			level, info.short_src, info.currentline,
+			(info.name ? info.name : "<unknown>"), info.what);
+		++level;
+	}
+
+	return 1;
+}
+
+int main(int argc, char* argv[]){
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+	lua_atpanic(L, __luaErrorHook);
+	LuaAdapterEnvironment& la = LuaAdapter::LuaAdapterEnvironment::getInstance();
+	la.init(L);
+
+	LuaAdapter::luaadapter_test();
+}
--- LuaAdapter/Test/test_center.cpp
+++ LuaAdapter/Test/test_center.cpp
@@ -1,4 +1,4 @@
-#include "../Classes/LuaAdapter.hpp"
+
 #include "test_center.h"
 #include "construction_destruction.h"
 #include "simple_test.h"
@@ -6,31 +6,10 @@
 
 namespace LuaAdapter {
 
-	static int __luaErrorHook(lua_State* L) {
-
-		const string errorStr = lua_tostring(L, -1);
-
-		cout << errorStr << endl;
-		lua_Debug info;
-		int level = 0;
-		while (lua_getstack(L, level, &info)) {
-			lua_getinfo(L, "nSl", &info);
-			fprintf(stderr, "  [%d] %s:%d -- %s [%s]\n",
-				level, info.short_src, info.currentline,
-				(info.name ? info.name : "<unknown>"), info.what);
-			++level;
-		}
-
-		return 1;
-	}
+	
 
 	void luaadapter_test() {
-		lua_State* L = luaL_newstate();
-		luaL_openlibs(L);
-		lua_atpanic(L, __luaErrorHook);
-		LuaAdapterEnvironment& la = LuaAdapter::LuaAdapterEnvironment::getInstance();
-		la.init(L);
-
+		
 		simple_test::simple_test();
 	
 		extends_test::extends_test();
